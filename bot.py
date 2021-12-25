56b8c05778cb1857ca674f96f0a27f4dadf3ba3f
Ivan Alasheev <clearstap@mail.ru>
2021-12-25 18:59:32 +0300

    Add first revision of bot

--- bot.py
+++ bot.py
@@ -4,17 +4,18 @@ Default echo bot
 
 import os
 import telebot
+import bot_data
 
-bot = telebot.TeleBot(os.environ['SECRET_TOKEN'])
+bot = telebot.TeleBot(os.environ['SECRET_TOKEN'], parse_mode='MARKDOWN')
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     """Says hello."""
-    bot.reply_to(message, welcome_message)
+    bot.reply_to(message, bot_data.welcome_message)
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     """Echo every message."""
-    bot.reply_to(message, questions[int(message.text)])
+    bot.reply_to(message, bot_data.questions[int(message.text)])
 
 bot.infinity_polling()
