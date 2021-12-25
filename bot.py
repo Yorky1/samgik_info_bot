4510605cc7c3f3809c6c557758496188864cab07
Ivan Alasheev <clearstap@mail.ru>
2021-12-25 16:55:23 +0300

    Check working

--- bot.py
+++ bot.py
@@ -15,6 +15,6 @@ def send_welcome(message):
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     """Echo every message."""
-    bot.reply_to(message, message.text)
+    bot.reply_to(message, "hi")
 
 bot.infinity_polling()
