7524e6506d1e2c8ef3f999a4f54047106449aead
Ivan Alasheev <clearstap@mail.ru>
2021-12-25 20:06:37 +0300

    Change parse_mode

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import os
 import telebot
 import bot_data
 
-bot = telebot.TeleBot(os.environ['SECRET_TOKEN'], parse_mode='MARKDOWN')
+bot = telebot.TeleBot(os.environ['SECRET_TOKEN'],parse_mode='HTML')
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
@@ -16,6 +16,6 @@ def send_welcome(message):
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     """Echo every message."""
-    bot.reply_to(message, bot_data.questions[int(message.text)])
+    bot.reply_to(message, bot_data.questions[int(message.text) - 1])
 
 bot.infinity_polling()
