7516738e7ed8ad8ca29ed106e34de18681e10a47
Ivan Alasheev <clearstap@mail.ru>
2021-12-26 03:41:58 +0300

    Add menu

--- bot.py
+++ bot.py
@@ -4,28 +4,36 @@ Default echo bot
 
 import os
 import telebot
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 import bot_data
 
 bot = telebot.TeleBot(os.environ['SECRET_TOKEN'],parse_mode='HTML')
 
+def keyboard(key_type="Normal"):
+    """Keybourd for messages"""
+    markup = ReplyKeyboardMarkup(row_width=1)
+    if key_type == "Normal":
+        for question in bot_data.questions_answers:
+            markup.add(KeyboardButton(question))
+    return markup
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     """Says hello."""
-    bot.reply_to(message, bot_data.welcome_message)
+    bot.send_message(message.chat.id,bot_data.welcome_message,reply_markup=keyboard())
 
 def correct_message(message):
     """Check message correctness."""
-    return message.text.isdigit() and 1 <= int(message.text) <= 10
+    return message.text in bot_data.questions_answers
 
 @bot.message_handler(func=correct_message)
 def answer_question(message):
     """Answer question."""
-    bot.reply_to(message, bot_data.questions[int(message.text) - 1])
+    bot.reply_to(message, bot_data.questions_answers[message.text])
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     """Answer every incorrect message."""
-    bot.reply_to(message, 'Введи номер от 1 до 10, чтобы я смог ответить на твой вопрос.\
-        Чтобы узнать номера вопросов напиши /help')
+    bot.reply_to(message, 'Ты задал вопрос, на который я не знаю ответа :(')
 
 bot.infinity_polling()
